f8f6404cd8c428dd24b79b449b46ee6f1033ac95
seiko <seiko@DESKTOP-4OT3UEV>
2020-11-22 17:18:03 -0300

    agrego nuevo modulo verduleria

--- src/gestionBD/listas.java
+++ src/gestionBD/listas.java
@@ -25,16 +25,17 @@ public class listas {
 
         DefaultTableModel modeloventas = new DefaultTableModel();
 
-        modeloventas.addColumn("cod de venta");
-        modeloventas.addColumn("id producto");
-        modeloventas.addColumn("codigo descuento");
+        modeloventas.addColumn("folio");
+        modeloventas.addColumn("producto");
+        modeloventas.addColumn("descuento");
         modeloventas.addColumn("cantidad");
         modeloventas.addColumn("total");
-        modeloventas.addColumn("cod venta");
-        modeloventas.addColumn("cod vendedor");
-        modeloventas.addColumn("rut cliente");
+       // modeloventas.addColumn("cod venta");
+        modeloventas.addColumn("vendedor");
+        modeloventas.addColumn("cliente");
         modeloventas.addColumn("fecha");
         modeloventas.addColumn("hora");
+        modeloventas.addColumn("sucursal");
 
         String[] ventas = new String[10];
 
@@ -43,27 +44,31 @@ public class listas {
         try {
             con = Conexion.conectar();
             stm = con.createStatement();
-            rs = stm.executeQuery("SELECT detalle_venta.codigo_detalle_venta,"
-                    + " detalle_venta.id_producto, detalle_venta.codigo_descuento, "
-                    + "detalle_venta.cantidad,  detalle_venta.total,  "
-                    + "ventas.codigo_venta,  ventas.codigo_vendedor,  "
-                    + "ventas.rut_cliente, ventas.fecha_venta,   ventas.hora_venta "
-                    + "FROM detalle_venta JOIN    "
-                    + "ventas ON detalle_venta.codigo_venta = ventas.codigo_venta");
+            rs = stm.executeQuery("select detalle_venta.codigo_detalle_venta, productos.nombre_producto, \n"
+                    + "descuentos.descuento, detalle_venta.cantidad,  detalle_venta.total,  \n"
+                    + "vendedores.nombre_vendedor,  clientes.nombre_cliente, \n"
+                    + "ventas.fecha_venta, ventas.hora_venta, sucursales.nombre_sucursal\n"
+                    + "FROM detalle_venta\n"
+                    + "JOIN ventas ON detalle_venta.codigo_venta = ventas.codigo_venta\n"
+                    + "JOIN Clientes ON ventas.id_cliente = Clientes.id_cliente\n"
+                    + "JOIN Vendedores ON ventas.codigo_vendedor = Vendedores.codigo_vendedor \n"
+                    + "join sucursales ON  vendedores.codigo_sucursal = sucursales.codigo_sucursal\n"
+                    + "JOIN Descuentos ON detalle_venta.codigo_descuento = Descuentos.codigo_descuento\n"
+                    + "JOIN Productos ON detalle_venta.id_producto = Productos.id_producto");
 
             while (rs.next()) {
 
-                ventas[0] = rs.getString("codigo_detalle_venta");
-                ventas[1] = rs.getString("id_producto");
-                ventas[2] = rs.getString("codigo_descuento");
-                ventas[3] = rs.getString("cantidad");
-                ventas[4] = rs.getString("total");
-                ventas[5] = rs.getString("codigo_venta");
-                ventas[6] = rs.getString("codigo_vendedor");
-                ventas[7] = rs.getString("rut_cliente");
-                ventas[8] = rs.getString("fecha_venta");
-                ventas[9] = rs.getString("hora_venta");
-                
+                ventas[0] = rs.getString("detalle_venta.codigo_detalle_venta");
+                ventas[1] = rs.getString("productos.nombre_producto");
+                ventas[2] = rs.getString("descuentos.descuento");
+                ventas[3] = rs.getString("detalle_venta.cantidad");
+                ventas[4] = rs.getString("detalle_venta.total");
+                ventas[5] = rs.getString("vendedores.nombre_vendedor");
+                ventas[6] = rs.getString("clientes.nombre_cliente");
+                ventas[7] = rs.getString("ventas.fecha_venta");
+                ventas[8] = rs.getString("ventas.hora_venta");
+                ventas[9] = rs.getString("sucursales.nombre_sucursal");
+
                 modeloventas.addRow(ventas);
             }
             jventas.setModel(modeloventas);
@@ -73,7 +78,7 @@ public class listas {
         }
 
     }
-    
+
     public void mostrarclientes(JTable jclientes) {
         Connection con = null;
         Statement stm;
@@ -103,7 +108,7 @@ public class listas {
                 clientes[2] = rs.getString("correo_cliente");
                 clientes[3] = rs.getString("telefono_cliente");
                 clientes[4] = rs.getString("direccion_cliente");
-                
+
                 modelocliente.addRow(clientes);
             }
             jclientes.setModel(modelocliente);
